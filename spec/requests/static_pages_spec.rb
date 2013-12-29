ab39b3ec4a110b81ade65f7681fcfd4b66d09aeb
TotalBollocks <jqrainwater@yahoo.com>
2013-12-29 13:11:36 -0600

    Fixed mistake in tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "StaticPages" do
 
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
-			page.should have_selector('h1', :text => 'Help')
+			page.should have_selector('h1', :text => 'Home')
 		end
 
 		it "should have title with 'Home'" do
