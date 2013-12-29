789316197564063865d8156bb839b2de750d7ebd
TotalBollocks <jqrainwater@yahoo.com>
2013-12-29 13:00:52 -0600

    Added tests for static page titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,23 +3,41 @@ require 'spec_helper'
 describe "StaticPages" do
 
 	describe "Home page" do
+
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
-			page.should have_content('Sample App')
+			page.should have_selector('h1', :text => 'Help')
+		end
+
+		it "should have title with 'Home'" do
+			visit '/static_pages/home'
+			page.should have_selector('title', :text => 'Home')
 		end
 	end
 
 	describe "Help Page" do
+
 		it "should have the content 'help'" do
 			visit '/static_pages/help'
-			page.should have_content('Help')
+			page.should have_selector('h1', :text => 'Help')
+		end
+
+		it "should have title with 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('title', :text => 'Help')
 		end
 	end
 
 	describe "About Page" do
+
 		it "should have the content 'About Us" do
 			visit '/static_pages/about'
-			page.should have_content('About Us')
+			page.should have_selector('h1', :text => 'About')
+		end
+
+		it "should have title with 'About'" do
+			visit '/static_pages/about'
+			page.should have_selector('title', :text => 'About')
 		end
 	end
 end
